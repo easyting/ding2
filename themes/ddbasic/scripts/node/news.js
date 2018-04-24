ba0696ddc9eb70a47cda59a942d586531f37a885
Mihaela Stavila <ms@inleadmedia.com>
2018-04-24 12:00:40 +0300

    1597 by Inlead: Fix js file for news node in block.

--- themes/ddbasic/scripts/node/news.js
+++ themes/ddbasic/scripts/node/news.js
@@ -3,6 +3,16 @@
 (function($) {
   'use strict';
 
+    // Set height for news teasers.
+    $(window).bind('resize.ding_news_teaser', function (evt) {
+      var ding_news_teaser_height = 0;
+  
+      $('.node-ding-news.node-teaser').each(function (delta, view) {
+        ding_news_teaser_height = $(this).find('.inner').outerHeight();
+        $(this).height(ding_news_teaser_height);
+      });
+    });
+
   /**
    * Hover first item in view ding news with class "first-child-large"
    */
@@ -19,6 +29,31 @@
         if(!ddbasic.breakpoint.is('mobile')) {
           $(this).find('.field-name-field-ding-news-lead').height(0);
         }
+      });
+    }
+  };
+
+  // Hover functions for news teasers.
+  Drupal.behaviors.ding_news_teaser_hover = {
+    attach: function(context, settings) {
+      var title_and_lead_height,
+          hovered;
+      $('.node-ding-news.node-teaser', context).mouseenter(function() {
+        // Set height for title and lead text.
+        title_and_lead_height = $(this).find('.title').outerHeight(true) + $(this).find('.field-name-field-ding-news-lead').outerHeight(true) + 20;
+        
+        $(this).find('.title-and-lead').css('min-height', title_and_lead_height);
+
+        // Set timeout to make shure element is still above while it animates
+        // out.
+        hovered = $(this);
+        setTimeout(function(){
+          $('.node-ding-news.node-teaser').removeClass('is-hovered');
+          hovered.addClass('is-hovered');
+        }, 300);
+      });
+      $('.node-ding-news.node-teaser').mouseleave(function() {
+         $(this).find('.title-and-lead').css('min-height', '');
       });
     }
   };
